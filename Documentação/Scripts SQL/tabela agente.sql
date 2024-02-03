3d726ce89c54f6ba7f81516a5ed5e30a27a9af68
Emanuelle Ferraz <manu.ferraz16@gmail.com>
2024-02-03 11:50:07 -0300

    add documents

---
+++ Documentação/Scripts SQL/tabela agente.sql
@@ -0,0 +1,14 @@
+use controle_doencas_bairro;
+INSERT INTO agente(CPF, Nome, Telefone, Senha)
+VALUES ('123321231-01', 'Camila Ribeiro Lima', '(31) 98733-3434', md5('camila123')),
+	   ('456654564-02', 'Julia da Costa Silva', '(31) 99999-8778', md5('julia456')),
+       ('789987897-03', 'Lucas Almeida de Souza', '(31) 98763-3241', md5('lucas789')),
+       ('890098908-04', 'Alana Soares Brito', '(31) 99456-8798', md5('alana123')),
+       ('098890980-05', 'Maria do Carmo Oliveira', '(31) 97654-9898', md5('maria456')),
+       ('987789897-06', 'Joaquim Rodrigues Pinheiro', '(31) 99715-2233', md5('joaquim789')),
+       ('654456564-07', 'Vitória Andrade Camargo', '(31) 97456-0987', md5('vick123')),
+       ('321123213-08', 'Daniela Carvalho Pinto', '(31) 98512-2334', md5('dani456')),
+       ('231123456-09', 'Carlos Emanuel Silva', '(31) 99912-8750', md5('carlos789')),
+       ('546789509-10', 'Barbara Aguiar Santos', '(31) 97045-9966', md5('barbie123')),
+       ('076543218-11', 'Ana Maria Sousa Miranda', '(31) 98908-2134', md5('aninha456')),
+       ('109876543-12', 'Gabriela Gomes Ribeiro ', '(31) 99976-3456', md5('gabie789'));
---
+++ Documentação/Scripts SQL/tabela doencas(v2).sql
@@ -0,0 +1,13 @@
+use controle_doencas_bairro;
+INSERT INTO doenca(Nome, Gravidade, Classificacao, Condicao)
+VALUES 
+('Acidente Vascular Cerebral (AVC)', 'Alta', 'Comum', 'Dor de cabeça repentina e forte, perna visão alterada,  dificuldade de coordenação;  confusão, alteração da fala ou compreensão. '),
+('Asma', 'Baixa', 'Comum', 'Falta de ar, tosse seca ou com secreções e chiado e aperto no peito.'),
+('Diabetes', 'Média', 'Comum', 'Perda de peso, fadiga, mudança de humor e fome frequente'),
+('Difteria', 'Média', 'Comum', 'Dor de garganta, febre, inchaço dos gânglios linfáticos e fraqueza.'),
+('Febre amarela', 'Média', 'Comum', 'Febre, dor de cabeça, náuseas e vômitos, doenças cardíacas, hepáticas e renais fatais.'),
+('Gastroenterite eosinofílica', 'Alta', 'Rara', 'Edema, náusea, inchaço, indigestão, fezes sangrentas.'),
+('Hipertensão arterial', 'Baixa', 'Comum', 'Dor de cabeça, mal-estar, tontura, dor no peito,  fraqueza,  visão embaçada.'),
+('Poliomielite Não Paralítica', 'Baixa', 'Rara', 'Febre, dor de garganta, dor de cabeça, fadiga.'),
+('Tétano neonatal', 'Alta', 'Rara', 'Espasmos musculares intermitentes, músculos rígidos, baba, constrição mandibular, dificuldade em engolir, dor de cabeça, espasmo com costas e pescoço arqueados');
+
